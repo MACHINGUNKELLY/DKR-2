10c469e4658011f6f145d6c28e9c7a9ac3218145
MACHINGUNKELLY <117808504+MACHINGUNKELLY@users.noreply.github.com>
2022-12-17 12:25:49 +0300

    Add files via upload

---
+++ дкрк 2-1.pas
@@ -0,0 +1,15 @@
+﻿const nmax=1000;
+begin
+ var n,k:integer;
+ var   a:array [1..nmax] of integer;
+ print('Введите количество элементов массива:');
+ read(n);
+ print('Введите значения элементов массива:');
+for var  i:integer:=1 to n do
+  readln(a[i]);
+ k:=1;
+  for var  i:integer:=2 to n do
+   if a[i-1]<>a[i]
+    then k:=k+1;
+ print(k)
+end.
---
+++ дкрк2-2.pas
@@ -0,0 +1,34 @@
+﻿begin
+var
+  s, ss: String;
+var  i, j, p, q: Integer;
+var  t: Char;
+  Write('   Строка: ');
+  ReadLn(s);
+  Write('Подстрока: ');
+  ReadLn(ss);
+  i:= 1;
+  j:= Length(s)-Length(ss)+1;
+  while i <= j do
+    begin
+    if Copy(s,i,Length(ss))=ss then
+      begin
+        p:= i;
+        Inc(i,Length(ss));
+        q:=i-1;
+        while p < q do
+          begin
+            t:=s[p];
+            s[p]:= s[q];
+            s[q]:= t;
+            Inc(p);
+            Dec(q);
+          end;
+        end
+      else 
+        Inc(i);
+  end;
+  Write('Результат: ');
+  print(s);
+  Readln;
+end.
